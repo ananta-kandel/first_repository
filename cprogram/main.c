6eac9b2451db60419596f2e9ed8e01bf9bb92400
Ananta kandel <kandelananta12@gmail.com>
2020-08-18 11:08:09 +0545

    hello

---
+++ cprogram/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enyter the number:");
+    scanf("%d",&a);
+    printf("Enter the anothrer number:");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The answer is %d",c);
+    
+    return 0;
+}
